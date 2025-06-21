072fdfe5d369a294ab81403dd33291c41f6ff462
radensuper <radenspot@gmail.com>
2025-06-21 07:12:19 +0700

    feat(ui): Rombak total UI/UX halaman login menjadi modern dan responsif

--- kbm_project/OLD_kbm_core/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KbmCoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kbm_core'
--- kbm_project/OLD_kbm_core/views.py
+++
@@ -1,7 +0,0 @@
-#from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>Selamat datang di Aplikasi Pengelolaan KBM </h1><p>Tekno Multi Kreasi Project</p>")
